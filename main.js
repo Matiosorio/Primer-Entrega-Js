532ddb80719ec72d256fad30d849becdbd19bc80
Matias Osorio <matiasosorio@gmail.com>
2023-04-11 16:00:10 -0300

    Tercer commit: Se agregaron alerts de bienvenida

--- main.js
+++ main.js
@@ -9,14 +9,23 @@ let modelo = "";
 let descuento = 0;
 let precioFinal = 0;
 
+
 // Aplicar descuentos
 function calcularPrecio() {
+    alert(`¡Bienvenido a la tienda Online de Tinki Winki!`);
+    
+    alert(`Solo en esta tienda encontrará las camisetas originales de la Selección Argentina al mejor precio del mercado`);
+
+    alert(`Dependiendo la cantidad que compres, podes obtener descuentos de 10, 20 o 30%`);
+
+    alert('Comencemos!')
+
     //Se solicita info al cliente
-    cantidadCamisetas = parseInt(prompt("Ingrese cantidad de camisetas de la Selección que desea comprar:"));
+    cantidadCamisetas = parseInt(prompt("Ingresa la cantidad de camisetas de la Selección que deseas comprar:"));
 
     modelo = prompt("Ingrese modelo de camiseta (titular o suplente)");
     while (modelo !== 'titular' && modelo !== 'suplente') {
-        modelo = prompt("Por favor ingrese una opción valida");
+        modelo = prompt("Por favor ingresa una opción valida (titular o suplente)");
     }
 
     precioFinal = cantidadCamisetas * precioPorCamiseta;
