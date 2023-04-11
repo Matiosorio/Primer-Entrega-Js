fce7796fb9c4c171ccb5d0157a1bdbb100de715e
Matias Osorio <matiasosorio@gmail.com>
2023-04-11 14:34:43 -0300

    Segundo comit: Se corrige porcentaje en alert final y mensaje en modelo

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Pre Entrega 1
 
-//Se calcula el descuento por la compra de cantidad de camisetas de futbol
+//Se calcula el descuento por la compra de cantidad de camisetas de la selección
 
 // Definir variables
 const precioPorCamiseta = 24000;
@@ -12,16 +12,16 @@ let precioFinal = 0;
 // Aplicar descuentos
 function calcularPrecio() {
     //Se solicita info al cliente
-    cantidadCamisetas = parseInt(prompt("Ingrese cantidad de camisetas que desea comprar:"));
+    cantidadCamisetas = parseInt(prompt("Ingrese cantidad de camisetas de la Selección que desea comprar:"));
 
     modelo = prompt("Ingrese modelo de camiseta (titular o suplente)");
     while (modelo !== 'titular' && modelo !== 'suplente') {
-        modelo = prompt("Ingrese modelo de camiseta (titular o suplente)");
+        modelo = prompt("Por favor ingrese una opción valida");
     }
 
     precioFinal = cantidadCamisetas * precioPorCamiseta;
 
-    //Si se compran mas de 5 camisetas o mas pero menos de 10 el descuento es del 10%, si se compran 10 o mas pero menos de 15 el descuento es del 20% y si se compran 15 o mas el descuento es del 20%
+    //Si se compran mas de 5 camisetas o mas pero menos de 10 el descuento es del 10%, si se compran 10 o mas pero menos de 15 el descuento es del 20% y si se compran 15 o mas el descuento es del 30%
     if (cantidadCamisetas >= 5 && cantidadCamisetas < 10) {
         descuento = precioPorCamiseta * cantidadCamisetas * 0.1;
     } else if (cantidadCamisetas >= 10 && cantidadCamisetas < 15) {
@@ -34,10 +34,9 @@ function calcularPrecio() {
     precioFinal -= descuento;
 
     //Mostramos el resultado al usuario:
-    alert(`El precio final con un ${descuento}% de descuento es: $${precioFinal}`);
+    alert(`El precio final con un ${descuento / (cantidadCamisetas * precioPorCamiseta) * 100}% de descuento es: $${precioFinal}`);
 }
 
 //Llamamos a la función
 calcularPrecio();
 
-//console.log(`El precio final con un ${descuento}% de descuento es: $${precioFinal}`);
